f6a116f8673e9ebeb8692157ca4e9f1e832a328e
gelaws-hub <fadhilgelaws@gmail.com>
2025-05-11 23:10:51 +0700

    first commit, my blog site using sanity cms and nextjs

---
+++ apps/studio/sanity.cli.ts
@@ -0,0 +1,14 @@
+import { defineCliConfig } from "sanity/cli";
+
+const projectId = process.env.SANITY_STUDIO_PROJECT_ID;
+const dataset = process.env.SANITY_STUDIO_DATASET;
+const host = process.env.HOST_NAME;
+
+export default defineCliConfig({
+  api: {
+    projectId: projectId,
+    dataset: dataset,
+  },
+  studioHost: host && host !== "main" ? host : "turbo-start-sanity",
+  autoUpdates: false,
+});
